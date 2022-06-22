b3fa131fce9879150fcc842fbd3b50fa5c3eaa1d
KgaugeloEliasMatlou <eliasmatlou66@gmail.com>
2022-06-22 13:51:54 +0200

    Add project files.

---
+++ IntroToInterFaces2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        mark m = new mark();
+
+        m.Dothis();
+        ((Isecond)m).Dothis();
+
+        Isecond mm = new mark();
+        mm.Dothis();
+    }
+}
+public class mark : Ifirst, Isecond
+{
+    public void Dothis()
+    {
+        Console.WriteLine("Implementation of Ifirst.Dothis ");
+    }
+
+    int Isecond.Dothis()
+    {
+        Console.WriteLine("Implementation of Isecond.Dothis ");
+        return 6;
+    }
+}
+
+interface Ifirst
+{
+    void Dothis();
+    
+}
+interface Isecond
+{
+    int Dothis();
+}
